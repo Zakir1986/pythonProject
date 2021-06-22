f4a3018561956fa03883592befc0c0f2b323baab
Zakir1986 <zakir.mirshanov@mail.ru>
2021-06-22 14:48:40 +0300

    init Ntstcsacsac

--- main.py
+++ main.py
@@ -3,65 +3,33 @@ from bs4 import BeautifulSoup
 # from list import list
 from LsitTest import ListTetst
 import re
-
-
+f = open('text.txt', 'w')
 for url in ListTetst:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    cont = soup.find('div', class_='content-middle')
-    if cont is None:
-        print(url, 'Другой шаблон')
-        continue
-    buttons = cont.find_all('a', class_='button')
-    line = []
-    for item in buttons:
-        line.append(item.string)
-    print(url, line)
+    result = re.findall(r'https://player.vimeo.com/video/', str(soup))
 
-# # Обьем текста # # Обьем текста # # Обьем текста # # Обьем текста
-#     textContainer = soup.find('div', class_='seo-text-container').text
-#     textNameAsList = textContainer.split(' ')  # текст в тип список
-#
-# # Количество символов
-#     listAsStr = ''
-#     for i in textNameAsList:
-#         listAsStr += i
-#     characters = len(listAsStr)
-#     replaceText = listAsStr.replace(' ', '')
-#     # количество слов в тексте
-#     wordCount = len(textNameAsList)
-#
-#
-#     # Актуальные бонусы
-#     bonusActual = soup.find('div', attrs={'v-html': 'bonusActualHtml'})
-#     bonusActualCards = bonusActual.find_all('div', class_='bonus-card')
-#     i = 0
-#     for tag in bonusActualCards:
-#         i +=1
-#     resActual = str(i)
-#
-#
-#     # Другие бонусы
-#     bonusOtherHtml = soup.find('div', attrs={'v-html': 'bonusOtherHtml'})
-#     bonusOtherCards = bonusOtherHtml.find_all('div', class_='bonus-card')
-#     i = 0
-#     for tag in bonusOtherCards:
-#         i +=1
-#     resOther = str(i)
-#
-#     # Прошедшие акции
-#     bonusArchiveHtml = soup.find('div', attrs={'v-html': 'bonusArchiveHtml'})
-#     bonusArhivCards = bonusArchiveHtml.find_all('div', class_='bonus-card')
-#     i = 0
-#     for tag in bonusArhivCards:
-#         i +=1
-#     resArhive = str(i)
-#
-#     print(url, '|', resActual, '|', resOther, '|', resArhive, '|', characters, '|', wordCount)
+    if not result:
+        Video = 0
+    else:
+        Video = 1
 
+    items = soup.find('div', attrs={'itemtype': 'https://schema.org/FAQPage'})
+    if items is None:
+        FAQ = 0
+    else:
+        FAQ = 1
 
-
-
-
+    listTag = []
+    for tag in soup.find_all(re.compile('^h[1-6]$')):
+        res = tag.name
+        listTag.append(res)
+    if ('h2' and 'h3') not in listTag:
+        hierarchy = f'Внимание!{listTag}'
+    else:
+        hierarchy = f'{listTag}'
+    print(url, '|', Video, '|', FAQ, '|', hierarchy)
+    f.write(url + '|' + str(Video) + '|' + str(FAQ) + '|' + hierarchy)
+f.close()
 
 
